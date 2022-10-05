9a888dd752d99d1dd83aab90c5dff5d279130616
rkamysz <radoslaw.kamysz@gmail.com>
2022-10-05 18:10:50 +0200

    add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+error_exit()
+{
+    echo "Error: $1"
+    exit 1
+}
+
+# major | minor | patch
+TYPE=patch
+
+if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]
+  then
+    TYPE=$1
+elif [[ $1 == "major" ]] || [[ $1 == "minor" ]] || [[ $1 == "patch" ]]
+  then
+    TYPE=$1
+elif [[ $1 == "" ]]
+  then
+    TYPE=patch
+else
+    printf "Unknown update type \"%s\".\n- Please use \"major\" | \"minor\" | \"patch\"\n" "$1"
+    exit 1;
+fi
+
+# Build package
+yarn prepublish || error_exit "Prepublish failed"
+
+# Update version and commit
+npm version $TYPE || error_exit "Failed to set a new version of the package"
+
+# Push changed package.json
+git push origin || error_exit "Failed to submit changes to the repository"
+
+# publish package
+npm publish || error_exit "Failed to publish a new version"
