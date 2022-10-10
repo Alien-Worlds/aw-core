c29c165b8107c8386c8bc122fb30f7d5acb2cc96
rkamysz <radoslaw.kamysz@gmail.com>
2022-10-10 15:15:49 +0200

    use wildcard export

--- src/index.ts
+++ src/index.ts
@@ -5,43 +5,35 @@ export * from './api/api.enums';
 export * from './api/api.types';
 export * from './api/api.errors';
 export * from './config';
-export { DataSourceBulkWriteError } from './storage/domain/errors/data-source-bulk-write.error';
-export { DataSourceOperationError, OperationErrorType } from './storage/domain/errors/data-source-operation.error';
-export { InsertManyError } from './storage/domain/errors/insert-many.error';
-export { InsertError } from './storage/domain/errors/insert.error';
-export { Failure } from './architecture/domain/failure';
-export { QueryModel } from './architecture/domain/query-model';
-export { Result } from './architecture/domain/result';
-export { UseCase } from './architecture/domain/use-case';
-export { MessagingAmqSource } from './messaging/data/data-sources/messaging.amq.source';
-export { MessagingSource } from './messaging/data/data-sources/messaging.source';
-export { MessageDto } from './messaging/data/messaging.dtos';
-export { wait } from './messaging/data/messaging.utils';
-export { MessageRepositoryImpl } from './messaging/data/repositories/message.repository-impl';
-export { Message } from './messaging/domain/entities/message';
-export { InvalidMessageQueueError } from './messaging/domain/errors/invalid-message-queue.error';
-export { MessageKeyAlreadyTakenError } from './messaging/domain/errors/message-key-already-taken.error';
-export { MessageNotFoundError } from './messaging/domain/errors/message-not-found.error';
-export { UndefinedMessageRepositoryError } from './messaging/domain/errors/undefined-message-repository.error';
-export { ConnectionState } from './messaging/domain/messaging.enums';
-export { MessageRepository } from './messaging/domain/repositories/message.repository';
-export {
-  EosRpcSource,
-  GetTableRowsOptions,
-} from './rpc/data/data-sources/eos-rpc.source';
-export { SmartContractRepositoryImpl } from './rpc/data/repositories/smart-contract.repository-impl';
-export { SmartContractDataNotFoundError } from './rpc/domain/errors/smart-contract-data-not-found.error';
-export { CollectionMongoSource } from './storage/data/data-sources/collection.mongo.source';
-export { connectMongo } from './storage/data/data-sources/mongo.helpers';
-export { MongoSource } from './storage/data/data-sources/mongo.source';
-export { MongoAggregateParams, MongoFindQueryParams } from './storage/data/mongo.types';
-export { Process } from './workers/process';
-export {
-  WorkerMessage,
-  WorkerMessageHandler,
-  WorkerMessageOptions,
-  WorkerMessageType,
-} from './workers/worker-message';
-export { WorkerOrchestrator } from './workers/worker-orchestrator';
-export { getWorkersCount } from './workers/worker.utils';
+export * from './storage/domain/errors/data-source-bulk-write.error';
+export * from './storage/domain/errors/data-source-operation.error';
+export * from './storage/domain/errors/insert-many.error';
+export * from './storage/domain/errors/insert.error';
+export * from './architecture/domain/failure';
+export * from './architecture/domain/query-model';
+export * from './architecture/domain/result';
+export * from './architecture/domain/use-case';
+export * from './messaging/data/data-sources/messaging.amq.source';
+export * from './messaging/data/data-sources/messaging.source';
+export * from './messaging/data/messaging.dtos';
+export * from './messaging/data/messaging.utils';
+export * from './messaging/data/repositories/message.repository-impl';
+export * from './messaging/domain/entities/message';
+export * from './messaging/domain/errors/invalid-message-queue.error';
+export * from './messaging/domain/errors/message-key-already-taken.error';
+export * from './messaging/domain/errors/message-not-found.error';
+export * from './messaging/domain/errors/undefined-message-repository.error';
+export * from './messaging/domain/messaging.enums';
+export * from './messaging/domain/repositories/message.repository';
+export * from './rpc/data/data-sources/eos-rpc.source';
+export * from './rpc/data/repositories/smart-contract.repository-impl';
+export * from './rpc/domain/errors/smart-contract-data-not-found.error';
+export * from './storage/data/data-sources/collection.mongo.source';
+export * from './storage/data/data-sources/mongo.helpers';
+export * from './storage/data/data-sources/mongo.source';
+export * from './storage/data/mongo.types';
+export * from './workers/process';
+export * from './workers/worker-message';
+export * from './workers/worker-orchestrator';
+export * from './workers/worker.utils';
 export * from './utils';
