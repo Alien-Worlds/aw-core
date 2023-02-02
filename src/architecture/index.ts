a9f0fa3b3dd0837f87f23a67d787408a810f4d7a
rkamysz <radoslaw.kamysz@gmail.com>
2023-02-02 16:49:16 +0100

    export compose

--- src/architecture/index.ts
+++ src/architecture/index.ts
@@ -2,10 +2,12 @@ export * from './domain/failure';
 export * from './domain/query-model';
 export * from './domain/result';
 export * from './domain/use-case';
+export * from './domain/compose';
 export * from './domain/repository';
 export * from './domain/repository.errors';
 
 export * from './data/mapper';
 export * from './data/repository-impl';
+export * from './data/compose-impl';
 export * from './data/collection.source';
 export * from './data/collection.types';
