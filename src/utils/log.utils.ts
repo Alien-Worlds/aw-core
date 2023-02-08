cda7a8a2714a2eeee4d44c27de5b6862bb94cee9
rkamysz <radoslaw.kamysz@gmail.com>
2023-02-08 20:18:47 +0100

    add timestamp to logs

--- src/utils/log.utils.ts
+++ src/utils/log.utils.ts
@@ -1,5 +1,5 @@
 export const log = (...args: unknown[]) => {
   if (!process.env.NO_LOGS || Number(process.env.NO_LOGS) === 0) {
-    console.log(`process:${process.pid} | `, ...args);
+    console.log(`process:${process.pid} | ${new Date().toISOString()} ::`, ...args);
   }
 };
