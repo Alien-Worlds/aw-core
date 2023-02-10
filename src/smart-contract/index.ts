66b17287bce8ae48fafe5b449bff14958b48311e
rkamysz <radoslaw.kamysz@gmail.com>
2023-02-10 10:59:26 +0100

    export contract data entity

--- src/smart-contract/index.ts
+++ src/smart-contract/index.ts
@@ -2,3 +2,5 @@ export * from './smart-contract-service';
 export * from './smart-contract-service-impl';
 export * from './contract-delta';
 export * from './contract-action';
+export * from './contract-data.entity';
+export * from './contract-unkown-data.entity';
