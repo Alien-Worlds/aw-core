a344c55bcaafaf9a229e360a57b2a38be51c5c78
Ahmad Tirmazi <16874651+ahmadtirmazi@users.noreply.github.com>
2023-02-21 17:00:47 +0100

    refactor: smart contract data not found error fallback for bound

--- src/rpc/smart-contract-data-not-found.error.ts
+++ src/rpc/smart-contract-data-not-found.error.ts
@@ -1,6 +1,6 @@
 export class SmartContractDataNotFoundError extends Error {
-  constructor(data: { bound?: string, scope?: string, table?: string }) {
-    const { bound, scope, table } = data;
-    super(`Data with ${bound} in scope: '${scope}' table: '${table}' scope not found`);
+  constructor(data: { bound?: string, lower_bound?: string, upper_bound?: string, scope?: string, table?: string }) {
+    const { bound, upper_bound, lower_bound, scope, table } = data;
+    super(`Data with ${bound || upper_bound || lower_bound} in scope: '${scope}' table: '${table}' scope not found`);
   }
 }
