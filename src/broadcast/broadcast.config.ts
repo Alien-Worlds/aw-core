a6b37168a227ba25423d946a4f51fcc247759a49
rkamysz <radoslaw.kamysz@gmail.com>
2023-04-14 22:15:49 +0200

    add broadcast config prefix

--- src/broadcast/broadcast.config.ts
+++ src/broadcast/broadcast.config.ts
@@ -1,9 +1,12 @@
 import { ConfigVars } from '../config';
 import { BroadcastConfig } from './broadcast.types';
 
-export const buildBroadcastConfig = (configVars: ConfigVars): BroadcastConfig => ({
-  port: configVars.getNumberEnv('BROADCAST_PORT'),
-  host: configVars.getStringEnv('BROADCAST_HOST'),
-  driver: configVars.getStringEnv('BROADCAST_DRIVER'),
-  clientName: configVars.getStringEnv('BROADCAST_CLIENT_NAME'),
+export const buildBroadcastConfig = (
+  configVars: ConfigVars,
+  prefix = ''
+): BroadcastConfig => ({
+  port: configVars.getNumberEnv(`${prefix.toUpperCase()}BROADCAST_PORT`),
+  host: configVars.getStringEnv(`${prefix.toUpperCase()}BROADCAST_HOST`),
+  driver: configVars.getStringEnv(`${prefix.toUpperCase()}BROADCAST_DRIVER`),
+  clientName: configVars.getStringEnv(`${prefix.toUpperCase()}BROADCAST_CLIENT_NAME`),
 });
