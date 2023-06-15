0e71384c46c5d49e2b4a2da2d38e9d85a9c12a7b
rkamysz <radoslaw.kamysz@gmail.com>
2023-06-15 16:57:00 +0200

    update mapper interface

--- src/architecture/data/mapper.ts
+++ src/architecture/data/mapper.ts
@@ -9,7 +9,6 @@ export abstract class Mapper<EntityType = unknown, DocumentType = unknown> {
    * Converts a document from the database layer to a domain entity.
    *
    * @param {DocumentType} document The document from the database layer.
-   *
    * @returns {EntityType} The domain entity.
    *
    * @abstract
@@ -20,12 +19,20 @@ export abstract class Mapper<EntityType = unknown, DocumentType = unknown> {
    * Converts a domain entity from the business logic layer to a database document.
    *
    * @param {EntityType} entity The domain entity from the business logic layer.
-   *
    * @returns {DocumentType} The document for the database layer.
    *
    * @abstract
    */
   public abstract fromEntity(entity: EntityType): DocumentType;
+
+  /**
+   * Gets entity key mapping based on the provided key.
+   *
+   * @abstract
+   * @param {string} key - The key for which to get the entity key mapping.
+   * @returns {PropertyMapping} The mapping associated with the provided key.
+   */
+  public abstract getEntityKeyMapping(key: string): PropertyMapping;
 }
 
 /**
