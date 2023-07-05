fdbec70e86073097a984300fd6f9f9050821388d
rkamysz <radoslaw.kamysz@gmail.com>
2023-07-05 08:30:24 +0200

    export node-fetch

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import nodeFetch from 'node-fetch';
+
 export * from 'inversify';
 export * from './api';
 export * from './architecture';
 export * from './config';
 export * from './blockchain';
 export * from './utils';
+
+export const fetch = nodeFetch;
